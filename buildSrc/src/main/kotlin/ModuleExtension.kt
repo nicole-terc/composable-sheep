90651a55099fffa23901d15eda98759dca347911
Nicole Terc <nicolesterc@gmail.com>
2024-10-30 01:11:32 +0100

    Update compile version

--- buildSrc/src/main/kotlin/ModuleExtension.kt
+++ buildSrc/src/main/kotlin/ModuleExtension.kt
@@ -1,10 +1,10 @@
 object ModuleExtension {
-    const val compileSdkVersion = 31
+    const val compileSdkVersion = 34
     const val jvmTarget = "11"
 
     object DefaultConfigs {
         const val minSdkVersion = 23
-        const val targetSdkVersion = 31
+        const val targetSdkVersion = 34
         const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         const val defaultConsumerProguardFiles = "consumer-rules.pro"
         const val proGuardRules = "proguard-rules.pro"
